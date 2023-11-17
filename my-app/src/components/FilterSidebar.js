e4f64d0dd5e6f8b81a6351c056f0c4a90fb1cc4a
Daniel Bahrami <daniel@bahrami.dk>
2023-11-17 02:03:44 +0100

    Add FilterSidebar

---
+++ my-app/src/components/FilterSidebar.js
@@ -0,0 +1,14 @@
+// FilterSidebar.js
+import React from "react";
+import "./FilterSidebar.css";
+
+const FilterSidebar = () => {
+  return (
+    <aside className="filter-sidebar">
+      {/* Add filter options here */}
+      <div>Filter</div>
+    </aside>
+  );
+};
+
+export default FilterSidebar;
